d56b8707bc2ddeff1cf6816fd196ad3da4f8d40f
ishparsh <uprety.ishparsh@gmail.com>
2024-11-13 14:28:13 -0800

    Directory management

---
+++ matrix_template.js
@@ -0,0 +1,321 @@
+
+/**
+ * Matrix with row-major layout:
+ *  0       1       2       3
+ *  4       5       6       7
+ *  8       9       10      11
+ *  12      13      14      15
+ */
+class Mat4 {
+
+    constructor( data ) {
+        if( data == null ) {
+            this.data = [
+                1, 0, 0, 0,
+                0, 1, 0, 0,
+                0, 0, 1, 0,
+                0, 0, 0, 1,
+            ]
+        }
+        else {
+            this.data = data;
+        }
+    }
+
+    static identity() {
+        return new Mat4();
+    }
+
+    toString() {
+        var str_vals = this.data.map( function( val ) { return "" + val } )
+        var str = 
+            str_vals.slice( 0, 4 ).join(' ') + '; ' + 
+            str_vals.slice( 4, 8 ).join(' ') + '; ' +
+            str_vals.slice( 8, 12 ).join(' ') + '; ' +
+            str_vals.slice( 12, 16 ).join(' ');
+
+        return '[' + str + ']';
+    }   
+
+
+    /**
+     * Returns a rotation matrix in the XY plane, rotating by the given number of turns. 
+     * @param {number} turns amount to rotate by
+     * @returns {Mat4}  
+     */
+    static rotation_xy( turns ) {
+        
+        // return the rotation matrix
+        const radians = turns * 2 * Math.PI;
+        const cos = Math.cos(radians);
+        const sin = Math.sin(radians);
+
+        return new Mat4([
+            cos, sin, 0, 0,
+            -sin, cos, 0, 0,
+            0, 0, 1, 0,
+            0, 0, 0, 1
+        ]);
+    }
+
+    /**
+     * Returns a rotation matrix in the XZ plane, rotating by the given number of turns
+     * @param {number} turns amount to rotate by
+     * @returns {Mat4}  
+     */
+    static rotation_xz( turns ) {
+
+        // return the rotation matrix
+        const radians = turns * 2 * Math.PI;
+        const cos = Math.cos(radians);
+        const sin = Math.sin(radians);
+
+        return new Mat4([
+            cos, 0, sin, 0,  // Rotating the X and Y values
+            0, 1, 0, 0,
+            -sin, 0, cos, 0,      // Z remains unchanged
+            0, 0, 0, 1       // Homogeneous coordinates
+        ]);
+    }
+
+    /**
+     * Returns a rotation matrix in the YZ plane, rotating by the given number of turns
+     * @param {number} turns amount to rotate by
+     * @returns {Mat4}  
+     */
+    static rotation_yz( turns ) {
+        const radians = turns * 2 * Math.PI;
+        const cos = Math.cos(radians);
+        const sin = Math.sin(radians);
+
+        return new Mat4([
+            1, 0, 0, 0,
+            0, cos, sin, 0,
+            0, -sin, cos, 0,
+            0, 0, 0, 1
+        ]);
+        
+        // return the rotation matrix
+    }
+
+    static translation( dx, dy, dz ) {
+        
+        // return the translation matrix
+        return new Mat4([
+            1, 0, 0, dx,
+            0, 1, 0, dy,
+            0, 0, 1, dz,
+            0, 0, 0, 1
+        ]);
+    }
+
+    static scale( sx, sy, sz ) {
+        
+        // return the scaling matrix
+        return new Mat4([
+            sx, 0, 0, 0,
+            0, sy, 0, 0,
+            0, 0, sz, 0,
+            0, 0, 0, 1
+        ]);
+    }
+
+
+    /**
+     * Creates a frustum projection matrix
+     * @param {number} left - Left boundary of the frustum
+     * @param {number} right - Right boundary of the frustum
+     * @param {number} bottom - Bottom boundary of the frustum
+     * @param {number} top - Top boundary of the frustum
+     * @param {number} near - Near boundary of the frustum
+     * @param {number} far - Far boundary of the frustum
+     * @returns {Mat4} The frustum projection matrix
+     */
+    static frustum( left, right, bottom, top, near, far ) { 
+        // these scalars will scale x,y values to the near plane
+        let scale_x = 2 * near / ( right - left );
+        let scale_y = 2 * near / ( top - bottom );
+
+        // shift the eye depending on the right/left and top/bottom planes.
+        // only really used for VR (left eye and right eye shifted differently).  
+        let t_x = ( right + left ) / ( right - left );
+        let t_y = ( top + bottom ) / ( top - bottom );
+
+        // map z into the range [ -1, 1 ] linearly
+        const linear_c2 = 1 / ( far - near );
+        const linear_c1 = near / ( far - near );
+        // remember that the w coordinate will always be 1 before being fed to the vertex shader.
+        // therefore, anything we put in row 3, col 4 of the matrix will be added to the z.
+
+        // map z into the range [ -1, 1], but with a non-linear ramp
+        // see: https://learnopengl.com/Advanced-OpenGL/Depth-testing and
+        // https://www.scratchapixel.com/lessons/3d-basic-rendering/perspective-and-orthographic-projection-matrix/opengl-perspective-projection-matrix and
+        // http://learnwebgl.brown37.net/08_projections/projections_perspective.html
+        // for more info. (note, I'm using left-handed coordinates. Some sources use right-handed).
+        const nonlin_c2 = (far + near) / (far - near);
+        const nonlin_c1 = 2 * far * near / (far - near);
+
+        let c1 = nonlin_c1;
+        let c2 = nonlin_c2;
+
+        return new Mat4([
+            scale_x,    0,          t_x,    0,
+            0,          scale_y,    t_y,    0,
+            0,          0,          c2,     -c1,
+            0,          0,          1,      0, 
+        ]);
+    }
+
+    mul( right ) {
+        
+        // return the result of multiplication
+        let result = new Array(16).fill(0);
+        for (let row = 0; row < 4; row++) {
+            for (let col = 0; col < 4; col++) {
+                result[row * 4 + col] =
+                    this.rc(row, 0) * right.rc(0, col) +
+                    this.rc(row, 1) * right.rc(1, col) +
+                    this.rc(row, 2) * right.rc(2, col) +
+                    this.rc(row, 3) * right.rc(3, col);
+            }
+        }
+        return new Mat4(result);
+    }
+
+	// right multiply by column vector
+    transform( x, y, z, w ) {
+        return this.transform_vec( new Vec4( x, y, z, w ) );
+    }
+
+    transform_vec( vec ) {
+        
+        // return the transformed vector
+        const x = this.rc(0, 0) * vec.x + this.rc(0, 1) * vec.y + this.rc(0, 2) * vec.z + this.rc(0, 3) * vec.w;
+        const y = this.rc(1, 0) * vec.x + this.rc(1, 1) * vec.y + this.rc(1, 2) * vec.z + this.rc(1, 3) * vec.w;
+        const z = this.rc(2, 0) * vec.x + this.rc(2, 1) * vec.y + this.rc(2, 2) * vec.z + this.rc(2, 3) * vec.w;
+        const w = this.rc(3, 0) * vec.x + this.rc(3, 1) * vec.y + this.rc(3, 2) * vec.z + this.rc(3, 3) * vec.w;
+
+        return new Vec4(x, y, z, w);
+    }
+
+
+    rc( row, col ) {
+        return this.data[ row * 4 + col ]
+    }
+
+    // inverting a 4x4 matrix is ugly, there are 16 determinants we 
+    // need to calculate. Because it's such a pain, I looked it up:
+    // https://stackoverflow.com/questions/1148309/inverting-a-4x4-matrix
+    // author: willnode
+    inverse() {
+        // var A2323 = m.m22 * m.m33 - m.m23 * m.m32 ;
+        const A2323 = this.rc(2, 2) * this.rc(3, 3) - this.rc(2, 3) * this.rc(3, 2); 
+        
+        // var A1323 = m.m21 * m.m33 - m.m23 * m.m31 ;
+        const A1323 = this.rc(2, 1) * this.rc(3, 3) - this.rc(2, 3) * this.rc(3, 1);
+        
+        // var A1223 = m.m21 * m.m32 - m.m22 * m.m31 ;
+        const A1223 = this.rc(2, 1) * this.rc(3, 2) - this.rc(2, 2) * this.rc(3, 1);
+
+        // var A0323 = m.m20 * m.m33 - m.m23 * m.m30 ;
+        const A0323 = this.rc(2, 0) * this.rc(3, 3) - this.rc(2, 3) * this.rc(3, 0);
+
+        // var A0223 = m.m20 * m.m32 - m.m22 * m.m30 ;
+        const A0223 = this.rc(2, 0) * this.rc(3, 2) - this.rc(2, 2) * this.rc(3, 0);
+
+        // var A0123 = m.m20 * m.m31 - m.m21 * m.m30 ;
+        const A0123 = this.rc(2, 0) * this.rc(3, 1) - this.rc(2, 1) * this.rc(3, 0);
+
+        // var A2313 = m.m12 * m.m33 - m.m13 * m.m32 ;
+        const A2313 = this.rc(1, 2) * this.rc(3, 3) - this.rc(1, 3) * this.rc(3, 2);
+
+        // var A1313 = m.m11 * m.m33 - m.m13 * m.m31 ;
+        const A1313 = this.rc(1, 1) * this.rc(3, 3) - this.rc(1, 3) * this.rc(3, 1);
+
+        // var A1213 = m.m11 * m.m32 - m.m12 * m.m31 ;
+        const A1213 = this.rc(1, 1) * this.rc(3, 2) - this.rc(1, 2) * this.rc(3, 1);
+
+        // var A2312 = m.m12 * m.m23 - m.m13 * m.m22 ;
+        const A2312 = this.rc(1, 2) * this.rc(2, 3) - this.rc(1, 3) * this.rc(2, 2);
+
+        // var A1312 = m.m11 * m.m23 - m.m13 * m.m21 ;
+        const A1312 = this.rc(1, 1) * this.rc(2, 3) - this.rc(1, 3) * this.rc(2, 1);
+
+        // var A1212 = m.m11 * m.m22 - m.m12 * m.m21 ;
+        const A1212 = this.rc(1, 1) * this.rc(2, 2) - this.rc(1, 2) * this.rc(2, 1);
+
+        // var A0313 = m.m10 * m.m33 - m.m13 * m.m30 ;
+        const A0313 = this.rc(1, 0) * this.rc(3, 3) - this.rc(1, 3) * this.rc(3, 0);
+
+        // var A0213 = m.m10 * m.m32 - m.m12 * m.m30 ;
+        const A0213 = this.rc(1, 0) * this.rc(3, 2) - this.rc(1, 2) * this.rc(3, 0);
+        
+        // var A0312 = m.m10 * m.m23 - m.m13 * m.m20 ;
+        const A0312 = this.rc(1, 0) * this.rc(2, 3) - this.rc(1, 3) * this.rc(2, 0);
+
+        // var A0212 = m.m10 * m.m22 - m.m12 * m.m20 ;
+        const A0212 = this.rc(1, 0) * this.rc(2, 2) - this.rc(1, 2) * this.rc(2, 0);
+
+        // var A0113 = m.m10 * m.m31 - m.m11 * m.m30 ;
+        const A0113 = this.rc(1, 0) * this.rc(3, 1) - this.rc(1, 1) * this.rc(3, 0);
+        
+        // var A0112 = m.m10 * m.m21 - m.m11 * m.m20 ;
+        const A0112 = this.rc(1, 0) * this.rc(2, 1) - this.rc(1, 1) * this.rc(2, 0);
+        
+
+        const det = 
+        this.rc(0, 0) * ( this.rc(1, 1) * A2323 - this.rc(1, 2) * A1323 + this.rc(1, 3) * A1223 ) -
+        this.rc(0, 1) * ( this.rc(1, 0) * A2323 - this.rc(1, 2) * A0323 + this.rc(1, 3) * A0223 ) +
+        this.rc(0, 2) * ( this.rc(1, 0) * A1323 - this.rc(1, 1) * A0323 + this.rc(1, 3) * A0123 ) -
+        this.rc(0, 3) * ( this.rc(1, 0) * A1223 - this.rc(1, 1) * A0223 + this.rc(1, 2) * A0123 );
+
+        const dr = 1.0 / det;
+
+        return new Mat4( [
+            dr * ( this.rc(1, 1) * A2323 - this.rc(1, 2) * A1323 + this.rc(1, 3) * A1223 ),
+            dr *-( this.rc(0, 1) * A2323 - this.rc(0, 2) * A1323 + this.rc(0, 3) * A1223 ),
+            dr * ( this.rc(0, 1) * A2313 - this.rc(0, 2) * A1313 + this.rc(0, 3) * A1213 ),
+            dr *-( this.rc(0, 1) * A2312 - this.rc(0, 2) * A1312 + this.rc(0, 3) * A1212 ),
+
+            dr *-( this.rc(1, 0) * A2323 - this.rc(1, 2) * A0323 + this.rc(1, 3) * A0223 ),
+            dr * ( this.rc(0, 0) * A2323 - this.rc(0, 2) * A0323 + this.rc(0, 3) * A0223 ),
+            dr *-( this.rc(0, 0) * A2313 - this.rc(0, 2) * A0313 + this.rc(0, 3) * A0213 ),
+            dr * ( this.rc(0, 0) * A2312 - this.rc(0, 2) * A0312 + this.rc(0, 3) * A0212 ),
+
+            dr * ( this.rc(1, 0) * A1323 - this.rc(1, 1) * A0323 + this.rc(1, 3) * A0123 ),
+            dr *-( this.rc(0, 0) * A1323 - this.rc(0, 1) * A0323 + this.rc(0, 3) * A0123 ),
+            dr * ( this.rc(0, 0) * A1313 - this.rc(0, 1) * A0313 + this.rc(0, 3) * A0113 ),
+            dr *-( this.rc(0, 0) * A1312 - this.rc(0, 1) * A0312 + this.rc(0, 3) * A0112 ),
+
+            dr *-( this.rc(1, 0) * A1223 - this.rc(1, 1) * A0223 + this.rc(1, 2) * A0123 ),
+            dr * ( this.rc(0, 0) * A1223 - this.rc(0, 1) * A0223 + this.rc(0, 2) * A0123 ),
+            dr *-( this.rc(0, 0) * A1213 - this.rc(0, 1) * A0213 + this.rc(0, 2) * A0113 ),
+            dr * ( this.rc(0, 0) * A1212 - this.rc(0, 1) * A0212 + this.rc(0, 2) * A0112 ),
+        ] );
+    }
+
+    clone() {
+        let c = new Array(16);
+        for( let i = 0; i < 16; i++ ) { c[i] = this.data[i]; }
+        return new Mat4( c );
+    }
+	
+	toString() {
+		let pieces = [ '[' ];
+		
+		for( let row = 0; row < 4; row ++ ){
+			pieces.push( '[' );
+			
+			for( let col = 0; col < 4; col ++ ){
+				let i = row * 4 + col;
+				pieces.push( this.data[i] );
+			}
+			
+			pieces.push( ']' )
+		}
+		
+		pieces.push( ']' );
+		
+		return pieces.join( ' ' );
+	}
+}
