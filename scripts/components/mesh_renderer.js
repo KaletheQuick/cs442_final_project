25614347099c618263b650b8c73b2063734fded8
Ryan <ryandanver@gmail.com>
2024-11-28 17:07:33 -0800

    Relocate mesh loading code to resource manager, update mesh renderer to reflect this

--- scripts/components/mesh_renderer.js
+++ scripts/components/mesh_renderer.js
@@ -1,25 +1,29 @@
+// Holds the mesh data for a node, and prepares it for the renderer
+
 class MeshRenderer extends Component{
 	static all = [];
 	static typestring = "MeshRenderer";
 
-	constructor(parent_node, mesh_path) {
+	constructor(parent_node, mesh_name) {
 		// do metadata stuff, and add this renderer to the list of instances
 		super(parent_node);
 		this.type = MeshRenderer;
 		MeshRenderer.all.push(this);
 
+		// Key for the mesh list dictionary. Mesh object is not directly stored in the mesh renderer.
+		this.mesh_name = mesh_name; 
+
+		// NOTE: functionality moved to resource manager
 		// load the mesh data. Mesh will be null until loaded
-		this.mesh_path = mesh_path;
-		this.mesh = null;
-		Mesh.load_from_file(this.mesh_path, (loaded_mesh) => {
-			this.mesh = loaded_mesh;
-			renderer_init();
-			console.log(this.mesh);
-		});
+		// this.mesh = null;
+		// Mesh.load_from_file(this.mesh_path, (loaded_mesh) => {
+		// 	this.mesh = loaded_mesh;
+		// 	renderer_init();
+		// 	console.log(this.mesh);
+		// });
 	}
 
 	_process(delta) {
-		// what to do in here??
 	}
 
 	// combine all the mesh vertex data of each MeshRenderer into a single array
@@ -29,9 +33,13 @@ class MeshRenderer extends Component{
 		for(let index = 0; index < this.all.length; index++) {
 			let renderer = this.all[index];
 
+			// Fetch the mesh associated with the renderer from the resource manager
+			let mesh = ResourceManager.meshes[renderer.mesh_name];
+
 			// Do not add the mesh if it hasn't been loaded yet.
-			if(renderer.mesh !== null)
-				zuper_mesh = zuper_mesh.concat(renderer.mesh.to_indexed_render_array(index));
+			if(mesh !== undefined) {
+				zuper_mesh = zuper_mesh.concat(mesh.to_indexed_render_array(index));
+			}
 		}
 		return zuper_mesh;
 	}
--- scripts/main.js
+++ scripts/main.js
@@ -1,26 +1,34 @@
-
 var Input = InputSystem.start_listening();
 var scene = new Node("root"); // root node
 
+// SECTION Preload resources
+// Load all the meshes we're going to use
+ResourceManager.load_mesh_list([
+    "diamond.obj",
+    "d4.obj",
+]);
+console.log(ResourceManager.meshes);
+
+// !SECTION
 
 // SECTION Debug manual scene setup 
 // Should have the same look and layout 
 // as the screenshots on discord.
 
 var diamond = scene.create_child("diamond");
-diamond.add_component(new MeshRenderer(diamond, "meshes/diamond.obj"));
+diamond.add_component(new MeshRenderer(diamond, "diamond.obj"));
 diamond.position = new Vec4(0,0,0);
 //diamond.rotation = new Vec4(-0.1,0,0.2);
 diamond.add_component(new DebugRotator(diamond, 0, 0.1, 0));
 
 var die4_01 = diamond.create_child("die4_01");
-die4_01.add_component(new MeshRenderer(die4_01, "meshes/d4.obj"));
+die4_01.add_component(new MeshRenderer(die4_01, "d4.obj"));
 die4_01.position = new Vec4(3,0,0);
 die4_01.rotation = new Vec4(-0.4,0,0.2);
 die4_01.add_component(new DebugRotator(die4_01, 0, 0.1 ,0));
 
 var racecar = scene.create_child("racecar");
-racecar.add_component(new MeshRenderer(racecar, "meshes/d4.obj"));
+racecar.add_component(new MeshRenderer(racecar, "d4.obj"));
 racecar.position = new Vec4(0,0,5);
 racecar.add_component(new DebugMovement(racecar));
 //die4_02.add_component(new DebugRotator(die4_02, 0,0,0.1));
@@ -92,20 +100,4 @@ function toggle_debout() {
     debout_show = !debout_show;
     document.getElementById("console").style.display = debout_show ? "block" : "none";
 }
-// !SECTION
-
-// SECTION scene graph matrix test code
-// let root = new Node();
-// let child = root.create_child("");
-
-// // move the root
-// root.translate(20, 0, 0);
-// // move the child relative to the root
-// child.translate(2, 10, 0);
-// // rotate the root (will change the child's position)
-// root.rotate_roll(Math.PI);
-
-// root._process(0);
-// console.log("PARENT: " + root.model.toString() + "\nCHILD: " + child.model.toString());
-
 // !SECTION
