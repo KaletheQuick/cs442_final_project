164c5cbb12042fbf23e7ae9aef4d31feb80bc607
Walibara <56965416+Walibara@users.noreply.github.com>
2024-12-04 11:26:51 -0800

    Update cubemap.js

--- scripts/components/cubemap.js
+++ scripts/components/cubemap.js
@@ -2,8 +2,8 @@ function loadCubeMap (gl)  {
     let faceInfos = [
         { target: gl.TEXTURE_CUBE_MAP_POSITIVE_X, url: 'img/right_nebula.png' },
         { target: gl.TEXTURE_CUBE_MAP_NEGATIVE_X, url: 'img/left_nebula.png' },
-        { target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y, url: 'img/top_nebula.png' },
-        { target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, url: 'img/bottom_nebula.png' },
+        { target: gl.TEXTURE_CUBE_MAP_POSITIVE_Y, url: 'img/bottom_nebula.png' },
+        { target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Y, url: 'img/top_nebula.png' },
         { target: gl.TEXTURE_CUBE_MAP_POSITIVE_Z, url: 'img/front_nebula.png' },
         { target: gl.TEXTURE_CUBE_MAP_NEGATIVE_Z, url: 'img/back_nebula.png' },
     ];
